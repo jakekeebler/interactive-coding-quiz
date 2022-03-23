c9c7c20f44b51032a82b111fa526458b6b3a91e9
Jake Keebler <jakekeebler@gmail.com>
2022-03-22 23:20:58 -0400

    added html, css, js, README, and images.

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # interactive-coding-quiz
+
+# <Interactive Coding Quiz>
+
+## Description
+
+This website presents a quiz to the user. Upon starting the quiz, the user must answer 5 multiple choice questions under a time limit, losing time if they answer a question incorrectly. After taking the quiz, the user is then prompted to enter their initials to post their score to a leaderboard, which is stores thier score in local storage. Upon refreshing the website, the scores will persist thanks to local storage.
+
+---
+
+## Screenshots
+
+![plot](./assets/images/img1.png)
+![plot](./assets/images/img2.png)
+![plot](./assets/images/img3.png)
+![plot](./assets/images/img4.png)
+
+## Installation
+
+In order to download and run this project, you may:
+
+- Clone the repository through Git, then run the directory in Visual Studio Code. Afterwards, run index.html and launch through the web browser of your choice.
+- Download the .zip of the repository, then extract it. Afterwards, open the folder through Visual Studio Code, or launch the index.html file directly.
+- Alternatively, if you want to view only the website without having the files, you can view the website by following this link: https://jakekeebler.github.io/interactive-coding-quiz/
+
+---
+
+## Usage
+
+Test your coding knowledge in this quick quiz and post your score to the leaderboard! 
+
+---
+
+## Credits
+
+HTML/CSS/JS Code: Jared Keebler
+
+---
+
+## License
+
+No license required.
+
+---
+
---
+++ assets/js/script.js
@@ -0,0 +1,209 @@
+// Variables for score and which question
+var score = 0;
+var questionIndex = 0;
+
+// Variables for querySelector elements
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTime");
+var questionsDiv = document.querySelector("#questionsDiv");
+
+// Variables for timer 
+var secondsLeft = 91;
+var holdInterval = 0;
+var penalty = 10;
+
+// Create list in html that can be interacted with in JS
+var ulCreate = document.createElement("ul");
+
+// Variable containing content for questions
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+
+];
+
+// Begins the quiz and starts timer
+timer.addEventListener("click", function () {
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            secondsLeft--;
+            currentTime.textContent = "Time: " + secondsLeft;
+
+            if (secondsLeft <= 0) {
+                clearInterval(holdInterval);
+                endQuiz();
+                currentTime.textContent = "Time's up!";
+            }
+        }, 1000);
+    }
+    render(questionIndex);
+});
+
+// Renders questions and choices to page: 
+function render(questionIndex) {
+   
+    // Clears existing data 
+    questionsDiv.innerHTML = "";
+    ulCreate.innerHTML = "";
+
+    // For loops to loop through all info in array
+    for (var i = 0; i < questions.length; i++) {
+        var userQuestion = questions[questionIndex].title;
+        var userChoices = questions[questionIndex].choices;
+        questionsDiv.textContent = userQuestion;
+    }
+
+    // New for each for question choices
+    userChoices.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        questionsDiv.appendChild(ulCreate);
+        ulCreate.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
+}
+
+// Function to compare the selected element to the answer contained in var questions
+function compare(event) {
+    var element = event.target;
+
+    if (element.matches("li")) {
+
+        var createDiv = document.createElement("div");
+        createDiv.setAttribute("id", "createDiv");
+        // If question is correct:
+        if (element.textContent == questions[questionIndex].answer) {
+            score++;
+            createDiv.textContent = "Correct!";
+        // If question is incorrect:
+        } 
+        
+        else {
+            secondsLeft = secondsLeft - penalty;
+            createDiv.textContent = "Wrong!";
+        }
+
+    }
+
+    // Proceeds to next question in array:
+    questionIndex++;
+
+    if (questionIndex >= questions.length) {
+        // If the questionIndex exceeds the length of var questions, then end the quiz:
+        endQuiz();
+        createDiv.textContent = "End of quiz!" + " " + "You got  " + score + "/" + questions.length + " Correct!";
+    } 
+    
+    else {
+        render(questionIndex);
+    }
+    questionsDiv.appendChild(createDiv);
+
+}
+// Function will append last page
+function endQuiz() {
+    questionsDiv.innerHTML = "";
+    currentTime.innerHTML = "";
+
+    // Heading:
+    var createH1 = document.createElement("h1");
+    createH1.setAttribute("id", "createH1");
+    createH1.textContent = "Finished!"
+
+    questionsDiv.appendChild(createH1);
+
+    // Paragraph
+    var createP = document.createElement("p");
+    createP.setAttribute("id", "createP");
+
+    questionsDiv.appendChild(createP);
+
+    // Calculates time remaining and replaces it with score
+    if (secondsLeft >= 0) {
+        var timeRemaining = secondsLeft;
+        var createP2 = document.createElement("p");
+        clearInterval(holdInterval);
+        createP.textContent = "Your final score is: " + timeRemaining;
+        questionsDiv.appendChild(createP2);
+    }
+
+    // Label for Initials
+    var createLabel = document.createElement("label");
+    createLabel.setAttribute("id", "createLabel");
+    createLabel.textContent = "Enter your initials: ";
+
+    questionsDiv.appendChild(createLabel);
+
+    // Input for initials
+    var createInput = document.createElement("input");
+    createInput.setAttribute("type", "text");
+    createInput.setAttribute("id", "initials");
+    createInput.textContent = "";
+
+    questionsDiv.appendChild(createInput);
+
+    // Submit data
+    var createSubmit = document.createElement("button");
+    createSubmit.setAttribute("type", "submit");
+    createSubmit.setAttribute("id", "Submit");
+    createSubmit.textContent = "Submit";
+
+    questionsDiv.appendChild(createSubmit);
+
+    // Event listener to capture initials and local storage for initials and score
+    createSubmit.addEventListener("click", function () {
+        var initials = createInput.value;
+
+        if (initials === null) {
+
+            console.log("No value entered!");
+
+        } 
+        
+        else {
+            var finalScore = {
+                initials: initials,
+                score: timeRemaining
+            }
+            console.log(finalScore);
+            var allScores = localStorage.getItem("allScores");
+            if (allScores === null) {
+                allScores = [];
+            } else {
+                allScores = JSON.parse(allScores);
+            }
+            allScores.push(finalScore);
+            var newScore = JSON.stringify(allScores);
+            localStorage.setItem("allScores", newScore);
+
+            // Takes user to new page which displays highscores
+            window.location.replace("./highscores.html");
+        }
+    });
+
+}
+
+
